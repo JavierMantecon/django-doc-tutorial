5960e120e5ed70715bf361fe0055c2c71407b05a
JavierMantecon <jmantecon@freepik.com>
2024-12-22 17:40:24 +0100

    feat(main): Writing your first Django app, part 7

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     # fields = ["pub_date", "question_text"] # This is the default without grouping / fieldset
+#     fieldsets = [
+#         (None, {"fields": ["question_text"]}),
+#         ("Date information", {"fields": ["pub_date"]}),
+#     ]
+
+# Edit Choice in the same form where Question is edited
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
